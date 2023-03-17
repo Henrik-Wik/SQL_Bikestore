2a64374f182cbafb5b81d901f65d6f9947a27022
HenrikWik97 <henrik.wik@outlook.com>
2023-03-17 16:11:06 +0100

    Added stuff

--- Script1.sql
+++ Script1.sql
@@ -1,16 +1,15 @@
 
--- Total Revenue, add per year function
+-- Total Sales per year --
 
 SELECT 
     SUM(oi.quantity * oi.list_price * (1 - oi.discount)) AS total_revenue,
     YEAR(o.order_date) AS order_year
 FROM Bikestores.sales.order_items AS oi
 JOIN Bikestores.sales.orders AS o ON oi.order_id = o.order_id
-WHERE o.order_status = 4 -- only consider completed orders
-GROUP BY YEAR(o.order_date)
+WHERE o.order_status = 4 -- only consider completed orders --
+GROUP BY YEAR(o.order_date);
 
-
--- top 10 customers in terms of revenue, add per year function
+-- top 10 customers in terms of sales --
 
 SELECT 
     TOP 10 c.first_name +' '+ c.last_name AS customer_name, 
@@ -21,9 +20,9 @@ JOIN Bikestores.sales.orders AS o ON c.customer_id = o.customer_id
 JOIN Bikestores.sales.order_items AS oi ON o.order_id = oi.order_id
 where o.order_status = 4
 GROUP BY YEAR(o.order_date), c.customer_id, c.first_name, c.last_name 
-ORDER by revenue DESC
+ORDER by revenue DESC;
 
--- sales per month
+-- sales per month --
 
 SELECT  
     YEAR(o.order_date) AS order_year,
@@ -33,11 +32,12 @@ FROM Bikestores.sales.orders AS o
 JOIN Bikestores.sales.order_items AS oi ON oi.order_id = o.order_id
 WHERE o.order_status = 4
 GROUP BY YEAR(o.order_date), MONTH(o.order_date)
-ORDER BY order_year, order_month
+ORDER BY order_year, order_month;
 
--- revenue per category, Add per year function
+-- sales per category --
 
-SELECT 
+SELECT
+    TOP 10
     YEAR(o.order_date) AS order_year,
     c.category_name, SUM(oi.quantity * oi.list_price * (1 - oi.discount)) AS revenue
 FROM bikestores.production.categories AS c
@@ -48,3 +48,16 @@ WHERE o.order_status = 4
 GROUP BY YEAR (o.order_date), c.category_name
 ORDER BY revenue DESC;
 
+-- Sales per city --
+
+SELECT 
+    SUM(oi.quantity * oi.list_price * (1 - oi.discount)) AS total_revenue,
+    YEAR(o.order_date) AS order_year,
+    c.city
+FROM Bikestores.sales.orders AS o
+JOIN Bikestores.sales.order_items AS oi
+     ON o.order_id = oi.order_id
+JOIN Bikestores.sales.customers AS c
+     ON o.customer_id = c.customer_id
+WHERE o.order_status = 4 -- only consider completed orders --
+GROUP BY YEAR(o.order_date), c.city;
